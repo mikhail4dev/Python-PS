62e344837e8d14b4b7dd0014c8bcadc7004ced45
Mikhail Volohatyi <m1k4dev@yandex.ru>
2026-06-03 16:54:06 +0700

    Упражнение - максимальное значение

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
-age:int = int(input("Введите возраст"))
+first_num:float = float(input("Введите первое число"))
+second_num:float = float(input("Введите второе число"))
+third_num:float = float(input("Введите трете число"))
 
-print(age > 18 and age < 50)
+if first_num >= second_num and first_num >= third_num:
+    print(f"Наибольшее число: {first_num}")
+elif second_num >= first_num and second_num >= third_num:
+    print(f"Наибольшее число: {second_num}")
+else:
+    print(f"Наибольшее число: {third_num}")
