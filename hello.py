7f8ec515bedd26362eefd6a3cc159733fd40a9f9
Mikhail Volohatyi <m1k4dev@yandex.ru>
2026-06-03 17:33:50 +0700

    Упражнение - Лифт

--- hello.py
+++ hello.py
@@ -1,10 +1,16 @@
-first_num:float = float(input("Введите первое число"))
-second_num:float = float(input("Введите второе число"))
-third_num:float = float(input("Введите трете число"))
+floor:int = int(input("Выберите этаж: "))
 
-if first_num >= second_num and first_num >= third_num:
-    print(f"Наибольшее число: {first_num}")
-elif second_num >= first_num and second_num >= third_num:
-    print(f"Наибольшее число: {second_num}")
-else:
-    print(f"Наибольшее число: {third_num}")
+match floor:
+    case -1:
+        print("Подвал: здесь находится склад")
+    case 1:
+        print("Холл и ресепшен")
+    case _ if 2 <= floor <= 9:
+        if floor % 2 == 0:
+            print(f"Офиосный этаж: {floor}")
+        else:
+            print(f"Жилой этаж: {floor}")
+    case 10:
+        print("Технической этаж - вход запрещен")
+    case _:
+        print(f"Такого этажа не существует: {floor}")
