d5b8621b4cecb9a178267a6ef4f5782c9015164e
Mikhail Volohatyi <m1k4dev@yandex.ru>
2026-06-23 20:34:14 +0700

    Упражнение - Калькулятор

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-# Таблица умножения от 1 до 10
+# Калькулятор
 
-for i in range(1,11):
-    for k in range(1, 11):
-        print(f"{i * k:4}", end="")
-    print("\n")
+def calculate(a:float, b:float, operation:str):
+    
+    match operation:
+        case "+":
+            return a + b
+        case "-":
+            return a - b
+        case "*":
+            return a * b
+        case "/":
+            return a / b if b != 0 else print("Нельзя разделить на 0")
+        case _:
+            return "Не известная операция"
+
+a_num:float = float((input("Введите первое число: ")))
+b_num:float = float((input("Введите второе число число: ")))
+operator:str = str((input("Укажите операцию (+, -, *, /): ")))
+
+print(calculate(a_num, b_num, operator))
