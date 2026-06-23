8bcfce4ff18aeb7066370b037881930f622c13c2
Mikhail Volohatyi <m1k4dev@yandex.ru>
2026-06-23 18:48:06 +0700

    Домашнее задание - Циклы (реализовано меню приложения через бесконечный цикл

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -1,4 +1,4 @@
-cost = input("Введите стоимость покупки в формате <руб> руб <коп> коп: ").strip().lower()
+""" cost = input("Введите стоимость покупки в формате <руб> руб <коп> коп: ").strip().lower()
 
 if 'руб' not in cost:
     print("Некорректный формат суммы")
@@ -26,4 +26,19 @@ if not rubles.isdigit() or not penny.isdigit():
 final_cost = float(rubles + "." + penny)
 rub_symbol = chr(8381)
 
-print(f"{final_cost:.2f} {rub_symbol}")
+print(f"{final_cost:.2f} {rub_symbol}") """
+
+MENU_LIST = ("Добавить расход", "Показать все расходы", "Показать сумму и средний расход", "Удалить расход по номеру", "Выход")
+user_select = 1
+
+while user_select:
+    print(f"{"=" * 30}")
+    for i, item in enumerate(MENU_LIST):
+        if i + 1 == len(MENU_LIST):
+            print(f"{(i + 1) - len(MENU_LIST)} - {item}")
+        else:
+            print(f"{i + 1} - {item}")
+            print(f"{"-" * 30}")
+    print(f"{"=" * 30}")
+    user_select = int(input("Выберите действие из меню(укажите только цифру): "))
+
